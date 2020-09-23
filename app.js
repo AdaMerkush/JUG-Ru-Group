6df2465792cce81cac7bc115625db2737e2820d4
AdaMerkush <merksa2@gmail.com>
2020-09-23 10:03:44 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const buttonOrder = document.querySelector('.button_yellow');
+const buttonFuck = document.querySelector('.button_black');
+
+let buttonClick = function() {
+  document.querySelector('.question').classList.remove('.closed');
+}
